ccf00b5a0ddfdc1cd23963756b7ea9ff56bc4bfc
1046773 <1046773@lwsd.org>
2024-11-16 23:09:14 -0800

    done

--- Chapter 5 - Coding/5.4 - Random Numbers/AppMain.java
+++ Chapter 5 - Coding/5.4 - Random Numbers/AppMain.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class AppMain {
     // The program should...
     //  Uses at least two methods.
@@ -9,6 +11,25 @@ public class AppMain {
     //  Average = 4.32
 
     private static final int ROLLCOUNT = 100;
+    public static void main(String[] args) {
+        int[] diceSides = {4, 6, 8, 10, 12, 20};
+        int die = pickRandomDie(diceSides);
+        double average = rollDieAndCalculateAverage(die);
+        System.out.println("Die: d" + die);
+        System.out.printf("Average = %.2f\n", average);
+    }
 
-    // Your code goes here...
+    private static int pickRandomDie(int[] diceSides) {
+        Random random = new Random();
+        return diceSides[random.nextInt(diceSides.length)];
+    }
+
+    private static double rollDieAndCalculateAverage(int sides) {
+        Random random = new Random();
+        int total = 0;
+        for (int i = 0; i < ROLLCOUNT; i++) {
+            total += random.nextInt(sides) + 1;
+        }
+        return (double) total / ROLLCOUNT;
+    }
 }
